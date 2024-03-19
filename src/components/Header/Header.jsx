ecf0c864c7fb8e112e5e894d03a082b7efe3ff12
OHshajim <ajshajimmax@gmail.com>
2024-03-20 00:51:10 +0600

    files add

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Nav bar</h1>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
